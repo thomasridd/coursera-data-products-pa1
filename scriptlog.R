42bb351aeacbbbd7abf8b14cc94142101692a375
thomasridd <thomasridd@hotmail.co.uk>
2014-10-22 01:03:52 +0100

    Data acquired

--- scriptlog.R
+++ scriptlog.R
@@ -1,1 +1,10 @@
 install.packages('devtools')
+
+devtools::install_github('rstudio/shinyapps')
+
+library(shinyapps)
+
+
+shinyapps::setAccountInfo(name="thomasridd", token="F7A5BF77F7DE29A246DBC081B56FA46D", secret="srP4bKkK+nZucoedTWD/5tyDRhJESGLSzgsxtba1")
+
+deployApp()
